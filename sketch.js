766d475e1c2fb81b4fc8699b5455df49503bc78a
Yining Shi <ys2189@nyu.edu>
2018-01-31 02:52:12 -0500

    first commit

---
+++ sketch.js
@@ -0,0 +1,39 @@
+let net;
+let outputImgData;
+let outputImg;
+let outputImgContainer;
+let video;
+let modelReady = false;
+let startPredict = false;
+let nextReady = true;
+
+function setup() {
+  noCanvas();
+  video = createCapture(VIDEO);
+  video.size(200, 200);
+  video.hide();
+  net = new p5ml.TransformNet('models/rain_princess', modelLoaded);
+  outputImgContainer = createImg('images/input.jpg', 'image');
+  outputImgContainer.parent('output-container');
+}
+
+function draw() {
+  if (startPredict && modelReady) {
+    predict();
+  }
+}
+
+function modelLoaded() {
+  console.log('model loaded');
+  modelReady = true;
+}
+
+function togglePredicting() {
+  startPredict = !startPredict;
+}
+
+function predict() {
+  outputImgData = net.predict(video.elt);
+  outputImg = p5ml.array3DToImage(outputImgData);
+  outputImgContainer.elt.src = outputImg.src;
+}
